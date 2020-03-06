f379594f39d3ce958700e99cb0d491502b5dfd2e
Essam <kairyesam@gmil.com>
2020-03-06 11:40:47 +0200

    Second

--- app/src/main/java/com/essam/miwok_app/NumbersActivity.java
+++ app/src/main/java/com/essam/miwok_app/NumbersActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumbersActivity extends AppCompatActivity {
 
+    private List<String> nums;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        nums = new ArrayList<>();
+        nums.add("One");
+        nums.add("Two");
+        nums.add("Three");
+        nums.add("Four");
+        nums.add("Five");
+        nums.add("Six");
+        nums.add("Seven");
+        nums.add("Eight");
+        nums.add("Nine");
+        nums.add("Ten");
     }
 }
